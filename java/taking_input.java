7a40208dff8fb44813be98af55c3aea87435e457
ANSHPAL18 <palansh880@gmail.com>
2025-11-08 18:03:58 +0530

    c

--- java/taking_input.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class taking_input {
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-     String name=sc.nextLine();
-    System.out.println(name);
-    }
-}
