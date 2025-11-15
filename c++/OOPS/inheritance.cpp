c840937d4867b6b04c69ba1c2e05d32bcf23347f
ANSHPAL18 <palansh880@gmail.com>
2025-11-15 18:35:19 +0530

    Add files via upload

---
+++ c++/OOPS/inheritance.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class customer{
+    private:
+    int a;
+    protected:
+    int b;
+    public:
+    int c;
+};
+int main(){
+customer Ansh;
+// Ansh.b=10;
+// Ansh.a=10;
+Ansh.c=10;
+}
