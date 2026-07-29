e40833ff60a9a4535a3d9555682669dc29e13036
ANSHPAL18 <palansh880@gmail.com>
2026-07-29 23:56:19 +0530

    Delete c++/simple_interest.cpp

--- c++/simple_interest.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    float principle=10000,rate=12,time=3;
-   float simple_interst=(principle*rate*time)/100;
-    cout<<"simple interest= ";
-    cout<<simple_interst<<endl;
-}
